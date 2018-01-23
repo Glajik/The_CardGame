21fb44241f3b713ac3700518e99150b8f9bdc534
Viacheslav <glajik@gmail.com>
2018-01-23 16:33:56 +0200

    Add customRandom parameter in game.js and make run() more simple

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -3,55 +3,32 @@ import { l } from '../sequences/pairs-data'; // eslint-disable-line
 import { random, reverse, length, get } from '../sequences/lists';
 import make from '../src/game'; // eslint-disable-line
 
-describe('CardGame', () => {
-  it('should work 1', () => {
-    const cards = l(
-      cons('Костяная кочерга гробницы', () => 6),
-    );
-    const game = make(cards);
-    const log = game('John', 'Ada');
-
-    expect(length(log)).toBe(5);
-
-    const step1 = get(0, log);
-    expect(pairToString(car(step1))).toBe('(10, 10)');
-
-    const step2 = get(1, log);
-    expect(pairToString(car(step2))).toBe('(10, 4)');
-
-    const step3 = get(2, log);
-    expect(pairToString(car(step3))).toBe('(4, 4)');
-
-    const step4 = get(3, log);
-    expect(pairToString(car(step4))).toBe('(4, -2)');
-
-    const step5 = get(4, log);
-    expect(pairToString(car(step5))).toBe('(4, -2)');
+test('CardGame', () => {
+  let cardIndex = 2;
+  const cards = l(
+    cons('Костяная кочерга гробницы', () => 7),
+    cons('Памятный металл палача', health => Math.round(health * 0.8)),
+  );
+  const game = make(cards, (c) => {
+    cardIndex = cardIndex === 0 ? 1 : 0;
+    return get(cardIndex, c);
   });
+  const log = game('John', 'Ada');
 
-  it('should work 2', () => {
-    const cards = l(
-      cons('Разъяряющая осада отчаяния', () => 5),
-    );
-    const game = make(cards);
-    const log = game('Mike', 'Alan');
+  expect(length(log)).toBe(5);
 
-    expect(length(log)).toBe(5);
+  const step1 = get(0, log);
+  expect(pairToString(car(step1))).toBe('(10, 10)');
 
-    const step1 = get(0, log);
-    expect(pairToString(car(step1))).toBe('(10, 10)');
+  const step2 = get(1, log);
+  expect(pairToString(car(step2))).toBe('(10, 3)');
 
-    const step2 = get(1, log);
-    expect(pairToString(car(step2))).toBe('(10, 5)');
+  const step3 = get(2, log);
+  expect(pairToString(car(step3))).toBe('(2, 3)');
 
-    const step3 = get(2, log);
-    expect(pairToString(car(step3))).toBe('(5, 5)');
+  const step4 = get(3, log);
+  expect(pairToString(car(step4))).toBe('(2, -4)');
 
-    const step4 = get(3, log);
-    expect(pairToString(car(step4))).toBe('(5, 0)');
-
-    const step5 = get(4, log);
-    expect(pairToString(car(step5))).toBe('(5, 0)');
-  });
+  const step5 = get(4, log);
+  expect(pairToString(car(step5))).toBe('(2, -4)');
 });
-
--- src/game.js
+++ src/game.js
@@ -2,32 +2,25 @@ import { cons, car, cdr, toString as pairToString } from '../sequences/pairs'; /
 import { cons as consList, l, head, toString as listToString } from '../sequences/pairs-data'; // eslint-disable-line
 import { random, reverse } from '../sequences/lists';
 
-const run = (player1, player2, cards) => {
+const run = (player1, player2, cards, customRandom) => {
   const iter = (health1, name1, health2, name2, order, log) => {
-    // BEGIN (write your solution here)
-    if (health1 <= 0 || health2 <= 0) {
-      const logItem = cons(cons(health1, health2), 'Игра окончена');
+    
+    if (health1 <= 0) {
+      const scores = order === 1 ? cons(health1, health2) : cons(health2, health1);
+      const message = `Игра окончена! Победил игрок ${name2}!`;
+      const logItem = cons(scores, message);
       return consList(logItem, log);
     }
 
-    const card = random(cards);
+    const card = customRandom(cards);
     const cardName = car(card);
-    const damage = cdr(card)();
+    const damage = cdr(card)(health2);
   
-    if (order === 1) {
-      const message = `Игрок '${name1}' применил '${cardName}' против '${name2}' 
-      и нанес урон '${damage}'`;
-      health2 -= damage;
-      const logItem = cons(cons(health1, health2), message);
-      return iter(health1, name1, health2, name2, 2, consList(logItem, log));
-    } else {
-      const message = `Игрок '${name2}' применил '${cardName}' против '${name1}' 
-      и нанес урон '${damage}'`;
-      health1 -= damage;
-      const logItem = cons(cons(health1, health2), message);
-      return iter(health1, name1, health2, name2, 1, consList(logItem, log));
-    }
-    // END
+		const newHealth = health2 - damage;
+		const scores = order === 1 ? cons(health1, newHealth) : cons(newHealth, health1);
+		const message = `Игрок ${name1} применил ${cardName} и нанес игроку ${name2} урон ${damage}`;
+		const logItem = cons(scores, message);
+    return iter(newHealth, name2, health1, name1, order === 1 ? 2 : 1, consList(logItem, log));
   };
 
   const startHealth = 10;
@@ -35,6 +28,6 @@ const run = (player1, player2, cards) => {
   return reverse(iter(startHealth, player1, startHealth, player2, 1, l(logItem)));
 };
 
-export default cards =>
-  (name1, name2) =>
-    run(name1, name2, cards);
+export default (cards, customRandom) =>
+  (player1, player2) =>
+    run(player1, player2, cards, customRandom);
